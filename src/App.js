e05ed671627a73382a5b343f332df91b143910f5
enkronan <robin@tempura.se>
2020-05-30 15:56:58 +0200

    type: feature
    created actions for increment and decrement and imported to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement} from './actions';
 
 function App() {
+
+  const counter = useSelector(state => state.counter);
+  const logged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <h1>Counter: {counter} </h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {logged ? <h3>Valuable info I shoulnd't see</h3> : ''}
+      
     </div>
   );
 }
